0e6cb5aaaf42f82e6d0a5806c2fd07cc4087d7bb
Noah Holcombe <nholcom2@students.kennesaw.edu>
2018-07-12 14:21:43 -0400

    tabs

--- UPSParks/App_Start/BundleConfig.cs
+++ UPSParks/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace UPSParks
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lux.css",
                       "~/Content/site.css"));
         }
     }
